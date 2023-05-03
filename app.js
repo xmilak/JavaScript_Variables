f096d582d4f777566c071304d23ebc637ff20e86
xmilak <126503532+xmilak@users.noreply.github.com>
2023-05-03 19:46:02 -0400

    Done

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
-
 // Exercise 2
+const firstName = "Neven";
+let lastName = `Milakovic`;
+var age = 26;
 
+console.log(`Hi, my name is ${firstName} ${lastName} and I am ${age} years old`);
 // Exercise 3
+let language = `Javascript`;
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`The ${language} language was created in ${createdYear} and it is ${isCaseSensitive} that it is case sensitive.`);
+
+//Exercise 4
+let price = 19.99
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "patrick Rothuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = yes;
